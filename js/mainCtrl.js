a0f4b9da90af6294eec3eb672a81b1a491720385
Justin Schow <justin.schow@gmail.com>
2016-06-25 11:14:00 -0600

    first commit js

---
+++ js/mainCtrl.js
@@ -0,0 +1,11 @@
+angular
+  .module('assessment')
+  .controller('mainCtrl', function($scope, mainService) {
+    /*$scope.getData = function() {
+      mainService.shopData().then(function(response) {
+        $scope.data = response;
+      });
+    };
+    $scope.getData();*/
+    
+  }); // end mainCtrl
---
+++ js/mainService.js
@@ -0,0 +1,16 @@
+angular
+  .module('assessment')
+  .service('mainService', function($http, $q) {
+
+    /*var shopUrl = 'https://dev-assessment.firebaseio.com/products.json';
+
+    this.shopData = function() {
+      var deferred = $q.defer();
+      $http.get(shopUrl).then(function(response) {
+        var shopData = response.data;
+        deferred.resolve(shopData);
+      });
+      return deferred.promise;
+    };*/
+    
+  }); // end mainService
