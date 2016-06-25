48cc3611ad1455f09f0956f8888751c310edbdb6
Justin Schow <justin.schow@gmail.com>
2016-06-25 11:23:33 -0600

    second commit - original

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,11 +1,5 @@
 angular
   .module('assessment')
   .controller('mainCtrl', function($scope, mainService) {
-    /*$scope.getData = function() {
-      mainService.shopData().then(function(response) {
-        $scope.data = response;
-      });
-    };
-    $scope.getData();*/
-    
+  
   }); // end mainCtrl
--- js/mainService.js
+++ js/mainService.js
@@ -1,16 +1,5 @@
 angular
   .module('assessment')
-  .service('mainService', function($http, $q) {
+  .service('mainService', function() {
 
-    /*var shopUrl = 'https://dev-assessment.firebaseio.com/products.json';
-
-    this.shopData = function() {
-      var deferred = $q.defer();
-      $http.get(shopUrl).then(function(response) {
-        var shopData = response.data;
-        deferred.resolve(shopData);
-      });
-      return deferred.promise;
-    };*/
-    
   }); // end mainService
